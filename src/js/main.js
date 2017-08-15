31682c1382e7163ed7e709631c8fc50021aaf2c4
Michiel Kalkman <michiel@nosuchtype.com>
2017-08-15 17:17:15 +1000

    feat: Add zoom

--- src/js/main.js
+++ src/js/main.js
@@ -187,6 +187,9 @@ var svg = d3.select("svg"),
   width = +svg.attr("width"),
   height = +svg.attr("height");
 
+var g = svg.append("g")
+    .attr("class", "everything");
+
 var nodes_data = xflow.nodes;
 
 var simulation = d3.forceSimulation()
@@ -198,20 +201,24 @@ simulation
 
 const nodeColorFill = (d) => {
   if (d.nodetype === "flow") {
-    return "blue";
+    if (d.action == "start") {
+      return "green";
+    } else {
+      return "red";
+    }
   } else {
-    return "red";
+      return "blue";
   }
 }
 
 //draw circles for the nodes 
-var node = svg.append("g")
+var node = g.append("g")
   .attr("class", "nodes")
   .selectAll("circle")
   .data(nodes_data)
   .enter()
   .append("circle")
-  .attr("r", 5)
+  .attr("r", 15)
   .attr("fill", nodeColorFill);
 
 simulation.on("tick", tickActions );
@@ -223,41 +230,36 @@ var links_data = xflow.edges.map((tup)=> {
   };
 });
 
-
-
-//Create the link force 
-//We need the id accessor to use named sources and targets 
-
-var link_force =  d3.forceLink(links_data)
-  .id(function(d) { return d.id; })
-
-//Add a links force to the simulation
-//Specify links  in d3.forceLink argument   
-
+var link_force = d3.forceLink(links_data).id(d=>d.id);
 
 simulation.force("links",link_force)
 
-//draw lines for the links 
-var link = svg.append("g")
+var link = g.append("g")
   .attr("class", "links")
   .selectAll("line")
   .data(links_data)
   .enter().append("line")
-  .attr("stroke-width", 2);        
+  .attr("stroke-width", 3);        
 
 function tickActions() {
-  //update circle positions each tick of the simulation 
   node
     .attr("cx", function(d) { return d.x; })
     .attr("cy", function(d) { return d.y; });
 
-  //update link positions 
-  //simply tells one end of the line to follow one node around
-  //and the other end of the line to follow the other node around
   link
     .attr("x1", function(d) { return d.source.x; })
     .attr("y1", function(d) { return d.source.y; })
     .attr("x2", function(d) { return d.target.x; })
     .attr("y2", function(d) { return d.target.y; });
 
-}                    
+}
+
+var zoom_handler = d3.zoom()
+  .on("zoom", zoom_actions);
+
+zoom_handler(svg);     
+
+function zoom_actions(){
+  g.attr("transform", d3.event.transform)
+}
+
