17acbc6e613677f623989be03c6db24675cba054
Michiel Kalkman <michiel@nosuchtype.com>
2017-08-16 17:24:42 +1000

    feat: Class for xflowgraph

--- src/js/main.js
+++ src/js/main.js
@@ -307,127 +307,111 @@ const xflow_2 = {
   ]
 };
 
-var svg = d3.select("svg");
-var width = +svg.attr("width");
-var height = +svg.attr("height");
-
-var g = svg.append("g")
-    .attr("class", "everything");
-
-
-var nodes_data = xflow_2.nodes;
-
-var links_data = xflow_2.edges.map((tup)=> {
-  return {
-    "source": tup[0],
-    "target": tup[1]
-  };
-});
-
-var simulation = d3.forceSimulation()
-  .nodes(nodes_data);	
-
-var link_force = d3.forceLink(links_data).id(d=>d.id);
-
-var radius = 12;
-var gravity = 165;
-
-var collide_force = d3.forceCollide(radius).strength(gravity);   
-     
-simulation
-  .force("center_force", d3.forceCenter(width / 2, height / 2))
-  .force("charge_force", d3.forceManyBody().strength(-gravity))
-  .force("links", link_force)
-  .force("collide", collide_force);
-
-const nodeColorFill = (d) => {
-  if (d.nodetype === "flow") {
-    if (d.action == "start") {
-      return "lightgreen";
-    } else if (d.action == "branch") {
-      return "green";
-    } else {
-      return "red";
-    }
-  } else if (d.nodetype === "flox") {
-      return "pink";
-  } else {
-      return "blue";
-  }
-}
-
-//draw circles for the nodes 
-var node = g.append("g")
-  .attr("class", "nodes")
-  .selectAll("circle")
-  .data(nodes_data)
-  .enter()
-  .append("circle")
-  .attr("r", 5)
-  .attr("fill", nodeColorFill);
-
-/*
-  .append("circle")
-  .attr("r", 5)
-  .attr("fill", nodeColorFill);
-
-var node = g.append("g")
-  .attr("class", "nodes")
-  .selectAll("circle")
-  .enter()
-	.append("text")
-	.text(function (d) { return d.name; })
-	.style("text-anchor", "middle")
-	.style("fill", "#555")
-	.style("font-family", "Arial")
-	.style("font-size", 12)
-*/
-
-simulation.on("tick", tickActions );
-
-var link = g.append("g")
-  .attr("class", "links")
-  .selectAll("line")
-  .data(links_data)
-  .enter().append("line")
-  .attr("stroke-width", 2);        
-
-function tickActions() {
-  node
-    .attr("cx", function(d) { return d.x; })
-    .attr("cy", function(d) { return d.y; });
-
-  link
-    .attr("x1", function(d) { return d.source.x; })
-    .attr("y1", function(d) { return d.source.y; })
-    .attr("x2", function(d) { return d.target.x; })
-    .attr("y2", function(d) { return d.target.y; });
-
-}
-
-var zoom_handler = d3.zoom()
-  .on("zoom", zoom_actions);
-
-zoom_handler(svg);     
-
-function zoom_actions(){
-  g.attr("transform", d3.event.transform)
-}
-
 class XFlowGraph {
 
   constructor(svgSelector) {
-    this._svg = d3.select(svgSelector);
-    this._width = +this.svg.attr("width");
-    this._height = +this.svg.attr("height");
-    this._g = svg.append("g").attr("class", "everything");
-    this._edges = [];
-    this._nodes = [];
+    this.svg = d3.select(svgSelector);
+    this.init();
   }
 
-  addNode(node) {
+  init() {
+    this.width = +this.svg.attr("width");
+    this.height = +this.svg.attr("height");
+    this.g = this.svg.append("g").attr("class", "everything");
 
+    this.radius = 7;
+    this.gravity = 65;
+  }
+
+  setXFlow(xflow) {
+    this.xflow = xflow;
+
+    this.nodes = xflow.nodes;
+    this.links = xflow.edges.map((tup)=> {
+      return {
+        "source": tup[0],
+        "target": tup[1]
+      };
+    });
+
+    this.simulation = d3.forceSimulation().nodes(this.nodes);	
+
+    var link_force = d3.forceLink(this.links).id(d=>d.id);
+
+    var collide_force = d3.forceCollide(this.radius).strength(this.gravity);   
+     
+    this.simulation
+      .force("center_force", d3.forceCenter(this.width / 2, this.height / 2))
+      .force("charge_force", d3.forceManyBody().strength(-this.gravity))
+      .force("links", link_force)
+      .force("collide", collide_force);
+
+
+    const nodeColorFill = (d) => {
+      if (d.nodetype === "flow") {
+        if (d.action == "start") {
+          return "blue";
+        } else if (d.action == "branch") {
+          return "green";
+        } else {
+          return "red";
+        }
+      } else if (d.nodetype === "flox") {
+        return "pink";
+      } else {
+        return "lightgreen";
+      }
+    }
+
+    //draw circles for the nodes 
+    this.nodeG = this.g.append("g")
+      .attr("class", "nodes")
+      .selectAll("circle")
+      .data(this.nodes)
+      .enter()
+      .append("circle")
+      .attr("r", 5)
+      .attr("fill", nodeColorFill);
+
+    this.linkG = this.g.append("g")
+      .attr("class", "links")
+      .selectAll("line")
+      .data(this.links)
+      .enter().append("line")
+      .attr("stroke-width", 2);        
+
+    this.simulation.on("tick", tickActions );
+
+    let self = this;
+
+    function tickActions() {
+      self.nodeG
+        .attr("cx", function(d) { return d.x; })
+        .attr("cy", function(d) { return d.y; });
+
+      self.linkG
+        .attr("x1", function(d) { return d.source.x; })
+        .attr("y1", function(d) { return d.source.y; })
+        .attr("x2", function(d) { return d.target.x; })
+        .attr("y2", function(d) { return d.target.y; });
+
+    }
+
+    var zoom_handler = d3.zoom()
+      .on("zoom", zoom_actions);
+
+    zoom_handler(this.svg);     
+
+    function zoom_actions(){
+      self.g.attr("transform", d3.event.transform)
+    }
   }
 
 }
+
+const xflowGraph = new XFlowGraph("#g1");
+xflowGraph.setXFlow(xflow);
+
+const xflowGraph2 = new XFlowGraph("#g2");
+xflowGraph2.setXFlow(xflow_2);
 
